0d8e661d9b06b0e2c431ab710f78d3c7fa1a2e60
Tylar <murray.tylar@gmail.com>
2013-05-26 03:17:34 -0400

    input module and unit test added

---
+++ testInput.py
@@ -0,0 +1,24 @@
+from getInput import *
+
+print ' === check() === '
+# no in
+print '""=>'+check('')
+print '"\\t"=>'+check('\t')
+print '"\\n"=>'+check('\n')
+
+# single word
+print '"apple"=>'+check('apple')
+print '"grape."=>'+check('grape.')
+print '"orange?"=>'+check('orange?')
+print '"banana!"=>'+check('banana!')
+
+# sentences
+print '"strawberry, sucka."=>'+check('strawberry, sucka.')
+print '"is this kiwi?"=>'+check('is this kiwi?')
+
+# multiple sentences
+print '"gimmie dat melon. Thank you kindly."=>'+check('you gimmie dat melon? (you hand me "dat melon") Splendid! Thank you kindly.')
+
+print '\n === prompt() === '
+test = prompt()
+print 'your input is "' + test + '"'
