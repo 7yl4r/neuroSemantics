823f549ce866a62953cbeaec719144d98a162e81
Tylar <murray.tylar@gmail.com>
2013-05-26 05:08:00 -0400

    added d3 force-directed visualization

--- testTrainOnStmt.py
+++ testTrainOnStmt.py
@@ -1,7 +1,11 @@
 import network
 from trainOnStatement import *
+import vizHandler
 
 stmt = 'sky is blue'
 net = network.network()
 
 trainOnStatement(net,stmt)
+
+v = vizHandler.viz()
+v.showNet()
